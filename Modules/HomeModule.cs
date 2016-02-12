a68b73d9fa36c047c9b5ea52dcd82f9ae49a3059
Neil Larion <neil.larion@gmail.com>
2016-02-12 09:53:51 -0800

    Edit links in views to match HomeModule.cs links

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,18 @@ namespace ContactList
       Get["/"] = _ => {
           return View["index.cshtml"];
       };
+      Get["/create_contact"] = _ => {
+          return View["createContact.cshtml"];
+      };      
+      Get["/contacts_delted"] = _ => {
+          return View["deleteContacts.cshtml"];
+      };      
+      Post["/contact_created"] = _ => {
+          return View["contactCreated.cshtml"];
+      };
+      Get["/list_contacts"] = _ => {
+          return View["listContacts.cshtml"];
+      };
     }
   }
 }
